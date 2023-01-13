dc14d85660b4a0bb33851dc780e77b2ccd851683
Félix Jouvhomme <felix.jouvhomme@gmail.com>
2023-01-13 17:15:03 +0100

    Fixed intro top padding

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -9,7 +9,7 @@ const Intro = () => {
 
 
     return (
-        <div className='flex items-center justify-center flex-col text-center pt-20 pb-6'>
+        <div className='flex items-center justify-center flex-col text-center pb-6'>
             <h1 className='text-4xl md:text-7xl mb-1 md:mb-3 font-bold dark:text-white mt-16'>Jouvhomme Félix</h1>
             <p className='text-base md:text-xl mb-3 font-medium'>{language === "FR" ? "Développeur web" : "Web Developer"}</p>
             {language === "FR" ? (<p className='text-sm max-w-xl mb-6 font-bold'>
