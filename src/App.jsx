8f3c99fcd310e7f67ff37a3883ed617a09485c1a
Félix Jouvhomme <felix.jouvhomme@gmail.com>
2023-01-16 16:59:40 +0100

    SYLVAIN FEEDBACK

--- src/App.jsx
+++ src/App.jsx
@@ -88,6 +88,14 @@ function App() {
 
   )
 
+  const traductorEN = (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 21l5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 016-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 01-3.827-5.802" />
+    </svg>
+
+
+  )
+
 
 
 
@@ -97,7 +105,7 @@ function App() {
     <>
       <appContext.Provider value={{ language, setLanguage }}>
         <button type="button" onClick={handleThemeSwitch} className="fixed p-2 z-50 right-10 top-5 sm:right-10 sm:top-5 dark:bg-dark-blue text-lg p-1 bg-blue-green rounded-md"> {theme === 'dark' ? sun : moon}</button>
-        <button type="button" onClick={handleLanguage} className="fixed p-2 z-50 right-20 top-5 sm:right-20 sm:top-5 dark:bg-dark-blue text-lg p-1 bg-blue-green rounded-md"> {traductor}</button>
+        <button type="button" onClick={handleLanguage} className="fixed p-2 z-50 right-20 top-5 sm:right-20 sm:top-5 dark:bg-dark-blue text-lg p-1 bg-blue-green rounded-md"> {language === "EN" ? traductorEN : traductor}</button>
         <div className="dark:text-stone-300 text-stone-900 min-h-screen font-inter relative">
           <div className={`spacer ${botLayer}`}></div>
 
